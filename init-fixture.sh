30f018244f555ec92943843c92fe42a497d1b508
Maxence Mottard Ynov <mottard.maxence@gmail.com>
2018-12-17 17:17:25 +0100

    Création des fixtures

--- init-fixture.sh
+++ init-fixture.sh
@@ -2,11 +2,17 @@
 
 rm -Rf ./fixtures
 
-mkdir -p ./fixtures/images ./fixtures/html ./fixtures/bazar
+mkdir -p ./fixtures/images ./fixtures/html ./fixtures/bazar ./fixtures/videos
+mkdir -p  ~/Scripts/fixtures/bazar/12
 
+wget https://filmpipeline.com/wp-content/uploads/sites/6/2017/08/ShortScriptContestIcon.png -q -O ./fixtures/bazar/images/picture.jpg
 wget https://twitter.com/ \
      https://media.melty.fr/article-3817747-thumb-f8/the-walking-dead-saison-9-episode-7-ce-soir.jpg \
       -q -P ./fixtures/bazar
 
-du -a ./fixtures
+echo "Hello World, my script is ok, i'm really happy !"			 > ./fixtures/success.txt
+echo "Continue the end is not far away." 				 >> ./fixtures/success.txt
 
+touch ./fixtures/videos/video.mp4
+
+du -a ./fixtures
--- start-script.sh
+++ start-script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-find ./fixtures *.jpg
+
+if [ ! -d ./fixtures ]
+then
+	echo "Je lance le script pour créer mes fixtures"
+	sh ./init-fixture.sh
+fi
+
+#find ./fixtures *.jpg
 
 
 
